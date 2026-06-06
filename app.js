9de628b553551d1e2645c85aa075dc1fa42b381c
Rohan-Revu <rohan.charandeeprevu@gmail.com>
2026-06-06 21:29:44 +0530

    Add root route and deployment fixes

--- app.js
+++ app.js
@@ -95,6 +95,8 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error.ejs", { message });
 });
 
-app.listen(8080, () => {
-  console.log("Server is running on port 8080");
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
